f030651ab79fd84cf9ea3c8b91b94c66b35fe234
sameerkumar2704 <shivamjnv01@gmail.com>
2024-12-30 16:40:17 +0530

    forgot password

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,12 +1,8 @@
 import { Input } from "@/components/ui/input";
+import { getDetails, postDetails } from "@/util/fetchHandlers";
 import { useRef, useState } from "react";
 
 export function ForgotPassword() {
-  const email = useRef();
-  return (
-    <div className='w-64 '>
-      <Input ref={email} type='email' />
-      <button onClick={() => console.log(email)}>Send</button>
-    </div>
-  );
+  // post resquest of forgot passwrod -> postDetails('/api/users/forgotpassword , {email:userEmail})
+  // after recived mail get the token form mail and send to postDetails('/api/users/resetPassword/:token , {password: new_password})
 }
